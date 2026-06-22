b788d4bbcf1b841ce50aa3421d5cfd4136321cc2
atharvingavale69-blip <atharvingavale69@gmail.com>
2026-06-23 01:04:03 +0530

    Update database.ts

--- config/database.ts
+++ config/database.ts
@@ -2,49 +2,23 @@ import path from 'path';
 import type { Core } from '@strapi/strapi';
 
 const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Database => {
-  const client = env('DATABASE_CLIENT', 'sqlite');
-
-  const connections = {
-    mysql: {
-      connection: {
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
-        },
-      },
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
-    },
-    postgres: {
-      connection: {
-        connectionString: env('DATABASE_URL'),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          rejectUnauthorized: false,
-        },
-      },
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
-    },
-    sqlite: {
-      connection: {
-        filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-      },
-      useNullAsDefault: true,
-    },
-  };
+  // Hum client ko hardcode 'postgres' kar rahe hain
+  const client = 'postgres';
 
   return {
     connection: {
       client,
-      ...connections[client],
-      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+      connection: {
+        // Yahan aapka database host, name, user, aur password hardcoded hai
+        host: 'amit-real-estate-db-jt2tsy',
+        port: 5432,
+        database: 'amit_db_strapi',
+        user: 'postgres',
+        password: 'Johny09', 
+        ssl: false,
+      },
+      pool: { min: 2, max: 10 },
+      acquireConnectionTimeout: 60000,
     },
   };
 };
